3b6bd2d06977a45ee34265cfb7fc180ce2930bcf
Darya Anikeeva <daanikeeva@gmail.com>
2022-02-24 11:08:15 +0300

    feat: add function check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let isTrue = [];
+  let stack = [];
+   for (let bracket of str) {
+     bracketsConfig.forEach(el => {
+        if (bracket == el[0] && bracket == el[1]) {
+           if (!stack.includes(bracket)) {
+              stack.push(el[1])
+           }
+           else {
+              if (stack.pop() !== bracket) {
+              isTrue.push(false)
+           }}
+        }
+        else if (bracket == el[0]) {
+           stack.push(el[1])
+        }
+        else if (bracket == el[1]) {
+           if (stack.pop() !== bracket) {
+              isTrue.push(false)
+           }
+        }
+     })
+   }
+  return (stack.length !== 0 || isTrue.includes(false)) ? false : true
 }
